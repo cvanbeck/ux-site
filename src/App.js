4325e6c8f7e79175452e39bdc9500ba1e726a72a
cvanbeck <charlievanbeck@gmail.com>
2023-12-11 18:56:08 +0000

    Added line of minirecipes for display on home page dynamically generated

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import { useState } from 'react'
-import MiniRecipe from "./components/MiniRecipe";
+import { MiniRecipeLine } from "./components/MiniRecipe";
 import recipesJson from "./recipes.json"
 
-
+const loadRecipePage = (recipe) => {
+  console.log(recipe)
+}
 
 function App() {
   const [recipes, setRecipes] = useState(recipesJson.recipeList) // Loads recupes into state
+  const [currentPage, setCurrentPage] = useState("homepage.js")
   const [favourites, setFavourites] = useState([])
-  console.log(recipes)
+  const [recipePage, setRecipePage] = useState(null)
 
+  const christmasSpecials = [recipes.chestnut, recipes.roastDinner]
+  const homemadeClassics = []
 
   return (
     <div>
-      <MiniRecipe recipe={recipes.chestnut}></MiniRecipe>
-      <MiniRecipe recipe={recipes.roastDinner}></MiniRecipe>
+      <MiniRecipeLine recipes={christmasSpecials} title={"Christmas Specials"} onClick={loadRecipePage}></MiniRecipeLine>
     </div>
   );
 }
--- src/components/MiniRecipe.js
+++ src/components/MiniRecipe.js
@@ -1,16 +1,26 @@
-const MiniRecipeLine = (recipes) => {
-    return
+export const MiniRecipeLine = ({recipes, title, onClick}) => {
+    return (
+        <div>
+            <h3>{title}</h3>
+            {recipes.map(recipe => 
+                <MiniRecipe
+                    key={recipe.id}
+                    recipe={recipe}
+                    onClick={onClick}/>
+
+                
+            )}
+        </div>
+    )
 }
 
-const MiniRecipe = ({ recipe }) => {
+export const MiniRecipe = ({ recipe, number, onClick }) => {
     return (
-        <button>
+        <button onClick={onClick} >
             <img src={`images/${recipe.image}`}alt={recipe.alt} width="200px" ></img>
-            <div id="number">{recipe.id}</div>
+            <div id="number">{number}</div>
             <div id="title">{recipe.recipename}</div>
             <div id="description">{recipe.brief}</div>
         </button>
     )
 }
-
-export default MiniRecipe
